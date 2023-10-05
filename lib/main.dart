14d3cff76e3821b310bf49a2f65da95ed5482e39
SPN 107 <spn107@spondonit.com>
2023-10-05 15:20:44 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
 import 'app/routes/app_pages.dart';
 import 'config/language/controller/language_controller.dart';
 import 'config/language/controller/language_selection.dart';
