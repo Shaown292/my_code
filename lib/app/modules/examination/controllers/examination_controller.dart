58532af587635a0b1367415af07687e6d010972f
spn107 <spn107@spondonit.com>
2023-10-31 15:45:54 +0600

    merge from dev_sifat1

--- lib/app/modules/examination/controllers/examination_controller.dart
+++ lib/app/modules/examination/controllers/examination_controller.dart
@@ -61,6 +61,7 @@ class ExaminationController extends GetxController {
   @override
   void onInit() {
     if(homeController.studentRecordList.isNotEmpty){
+
       getStudentExamList(recordId: homeController.studentRecordList[0].id);
 
     }
