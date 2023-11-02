16129514969e9ac90866471edfc0275e958a9601
spn107 <spn107@spondonit.com>
2023-11-02 08:35:19 +0600

    merge from dev_sifat1

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -35,7 +35,6 @@ class HomeController extends GetxController {
   void logout() async {
     LoadingController loadingController = Get.find();
     try {
-
       // var headers = {
       //   'Authorization': GlobalVariableController.token!,
       // };
