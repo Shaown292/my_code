b109746e9bbb38eb35c4bba2f82f4575fce4c34c
SPN 107 <spn107@spondonit.com>
2023-12-18 16:37:33 +0600

    package upgrade

--- lib/app/utilities/widgets/common_widgets/duplicate_dropdown.dart
+++ lib/app/utilities/widgets/common_widgets/duplicate_dropdown.dart
@@ -1,6 +1,4 @@
-import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_single_getx_api_v2/app/data/constants/app_colors.dart';
 import 'package:flutter_single_getx_api_v2/app/data/constants/app_text_style.dart';
 import 'package:flutter_single_getx_api_v2/app/utilities/extensions/widget.extensions.dart';
 import 'package:get/get.dart';
