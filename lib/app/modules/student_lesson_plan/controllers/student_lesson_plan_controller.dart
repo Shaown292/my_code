c20bbab9df774663bc502d37f57f0f34caabc374
SPN 107 <spn107@spondonit.com>
2023-12-28 09:26:49 +0600

    merging with dev_shaown

--- lib/app/modules/student_lesson_plan/controllers/student_lesson_plan_controller.dart
+++ lib/app/modules/student_lesson_plan/controllers/student_lesson_plan_controller.dart
@@ -298,6 +298,7 @@ class StudentLessonPlanController extends GetxController {
                           onChanged: (bool value) {
                             value =
                                 studentLessonPlanDetailsData.status ?? false;
+
                           }),
                     ],
                   ),
