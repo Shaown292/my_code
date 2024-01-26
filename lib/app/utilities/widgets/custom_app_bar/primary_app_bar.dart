42f142530466f68785213e0b40d6512f978e022b
Shaown <kzshaown@gmail.com>
2024-01-26 16:30:08 +0600

    Merging with dev_shaown2

--- lib/app/utilities/widgets/custom_app_bar/primary_app_bar.dart
+++ lib/app/utilities/widgets/custom_app_bar/primary_app_bar.dart
@@ -24,6 +24,7 @@ class PrimaryAppBar extends StatelessWidget {
                 width: 66,
                 decoration: const BoxDecoration(
                   image: DecorationImage(
+
                     image: AssetImage(ImagePath.appLogo),
                   ),
                 ),
